3448a57a292f3aa8af42da3ebc2002092dd79da0
masod ghanim <62252603+masod216@users.noreply.github.com>
2020-11-02 17:36:45 +0200

    Add files via upload

---
+++ 02_basic output.py
@@ -0,0 +1,10 @@
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="#", end="*\n")
+
+"""
+OUTPUT:
+___________________
+
+My_name_is*Monty#Python.*
+"""
+
---
+++ 08_calculator.py
@@ -0,0 +1,10 @@
+num1=int(input("Please enter first number: "))
+num2=int(input("Please enter second number: "))
+
+sum=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+mod=num1%num2
+
+print("num1:",num1," ,num2:",num2," ,sum:",sum," ,sub:",sub," ,mul:",mul," ,div:",div," ,mod:",mod)
