58e0a1a73495fbdc303415eef3ad608457447bda
masod ghanim <62252603+masod216@users.noreply.github.com>
2020-11-02 17:39:33 +0200

    Add files via upload

---
+++ tirgol.py
@@ -0,0 +1,17 @@
+class Car:
+
+    def increment(self):
+        print('increment')
+
+    def Decrement(self):
+        print('Decrement')
+
+BMW =  Car()
+BMW.speed = 240
+BMW.color = 'black'
+print(BMW.speed)
+
+camry = Car()
+camry.speed = 200
+camry.color = 'yellow'
+
---
+++ tirgol01.py
@@ -0,0 +1,18 @@
+class Car:
+
+    def increment(self):
+        print('increment')
+
+    def decrement(self):
+        print('Decrement')
+
+BMW =  Car()
+BMW.speed = 240
+BMW.color = 'black'
+BMW.model = 2017
+BMW.price = 12000
+print(BMW.speed)
+
+camry = Car()
+camry.speed = 200
+camry.color = 'yellow'
